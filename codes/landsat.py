8ae411aaacc55d28c4da2c302482ece19cd0d952
FidelBM <a01798199@tec.mx>
2024-10-05 12:20:39 -0600

    Refactor landsat.py to include a new function for retrieving pixel grid data

--- codes/landsat.py
+++ codes/landsat.py
@@ -3,6 +3,28 @@ ee.Authenticate()
 ee.Initialize(project='ee-fbonillamontalvo')
 
 
+def get_pixel_grid(latitude, longitude):
+    point = ee.Geometry.Point([longitude, latitude])
+    collection = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2') \
+        .filterBounds(point) \
+        .sort('CLOUD_COVER')
+
+    # Verificar si la colección contiene imágenes
+    if collection.size().getInfo() == 0:
+        print("No images found for the specified location and date range.")
+        return None
+
+    # Obtener la primera imagen de la colección
+    image = collection.first()
+
+    # Define la región de interés (90x90 metros en Landsat)
+    region = point.buffer(45).bounds()
+
+    # Tomar muestra de la imagen en la región especificada
+    data = image.sample(region, 30).getInfo()
+    return data
+
+
 def get_landsat_data(latitude, longitude, start_date, end_date):
     # Definir el punto de interés
     point = ee.Geometry.Point([longitude, latitude])
@@ -32,7 +54,7 @@ start_date = '2023-01-01'
 end_date = '2023-12-31'
 
 try:
-    url = get_landsat_data(latitude, longitude, start_date, end_date)
-    print('Download URL:', url)
+    data = get_pixel_grid(latitude, longitude)
+    print(data)
 except ee.EEException as e:
     print('Error:', e)
